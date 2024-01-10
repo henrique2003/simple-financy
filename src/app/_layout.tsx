0743c8d347e7757c62103e80371bbf85a52dea13
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2024-01-10 19:43:06 -0300

    feat: add intial config routes

---
+++ src/app/_layout.tsx
@@ -0,0 +1,75 @@
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
+import {
+  useFonts,
+  Poppins_100Thin,
+  Poppins_100Thin_Italic,
+  Poppins_200ExtraLight,
+  Poppins_200ExtraLight_Italic,
+  Poppins_300Light,
+  Poppins_300Light_Italic,
+  Poppins_400Regular,
+  Poppins_400Regular_Italic,
+  Poppins_500Medium,
+  Poppins_500Medium_Italic,
+  Poppins_600SemiBold,
+  Poppins_600SemiBold_Italic,
+  Poppins_700Bold,
+  Poppins_700Bold_Italic,
+  Poppins_800ExtraBold,
+  Poppins_800ExtraBold_Italic,
+  Poppins_900Black,
+  Poppins_900Black_Italic
+} from '@expo-google-fonts/poppins'
+
+export {
+  ErrorBoundary
+} from 'expo-router'
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout(): JSX.Element {
+  const [loaded] = useFonts({
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return <></>
+  }
+
+  return <RootLayoutNav />
+}
+
+function RootLayoutNav(): JSX.Element {
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ThemeProvider>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+// import { Container } from './styles';
+
+const Initial: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ola Simple finance</Text>
+    </View>
+  )
+}
+
+export default Initial
