c5bead2d25307fd445a5d98f37e72f5af277df76
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2024-01-10 20:17:29 -0300

    feat: add taillwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-// import { Container } from './styles';
-
 const Initial: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ola Simple finance</Text>
+    <View className='flex-1 justify-center items-center bg-white'>
+      <Text className='mt-10'>Ola Simple finance</Text>
     </View>
   )
 }
